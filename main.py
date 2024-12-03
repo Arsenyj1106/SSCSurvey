ea43e6cdb1a3019259a08c288125426f20a283e9
Арсений Пирогов <pirogovarceniy@vk.com>
2024-12-03 19:31:39 +0500

    Отрефакторил + добавил роут для /school-data

--- main.py
+++ main.py
@@ -1,190 +1,261 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from db.db import db
 from db.models import EducationalOrganization, Event
 
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key' # Key for sessions to store user data
+login = LoginManager(app)
+app.secret_key = "your_secret_key"  # Key for sessions to store user data
+login.login_view = "login"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sscsserverpostgres@sscsurvey.ru:5432/sscs_db?connect_timeout=10&sslmode=prefer'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Temporary user store for demo purposes
+users = {"admin": {"password": "password"}}
+
+
+# User class implementing UserMixin
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+    def get_id(self):
+        return self.id
+
+
+@login.user_loader
+def load_user(user_id):
+    if user_id in users:
+        return User(user_id)
+    return None
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql+psycopg2://postgres:sscsserverpostgres@sscsurvey.ru:5432/sscs_db?connect_timeout=10&sslmode=prefer"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-def save_survey_data():
-    block1_data = session.get('block1_data')
-    block2_data = session.get('block2_data')
-    block3_data = session.get('block3_data')
 
+def save_survey_data():
+    block1_data = session.get("block1_data")
+    block2_data = session.get("block2_data")
+    block3_data = session.get("block3_data")
     if block1_data:
         organization = EducationalOrganization(
-            students_number=block1_data['totalStudents'],
-            club_members_grade1=block1_data['class1'],
-            club_members_grade2=block1_data['class2'],
-            club_members_grade3=block1_data['class3'],
-            club_members_grade4=block1_data['class4'],
-            club_members_grade5=block1_data['class5'],
-            club_members_grade6=block1_data['class6'],
-            club_members_grade7=block1_data['class7'],
-            club_members_grade8=block1_data['class8'],
-            club_members_grade9=block1_data['class9'],
-            club_members_grade10=block1_data['class10'],
-            club_members_grade11=block1_data['class11']
+            students_number=block1_data["totalStudents"],
+            club_members_grade1=block1_data["class1"],
+            club_members_grade2=block1_data["class2"],
+            club_members_grade3=block1_data["class3"],
+            club_members_grade4=block1_data["class4"],
+            club_members_grade5=block1_data["class5"],
+            club_members_grade6=block1_data["class6"],
+            club_members_grade7=block1_data["class7"],
+            club_members_grade8=block1_data["class8"],
+            club_members_grade9=block1_data["class9"],
+            club_members_grade10=block1_data["class10"],
+            club_members_grade11=block1_data["class11"],
         )
         db.session.add(organization)
         db.session.commit()
-
     if block2_data:
         for event_data in block2_data.values():
             event = Event(
-                name=event_data['event_name'],
-                participants=event_data['participants'],
-                date_start=event_data['date_start'],
-                date_end=event_data['date_end'],
+                name=event_data["event_name"],
+                participants=event_data["participants"],
+                date_start=event_data["date"],
             )
             db.session.add(event)
         db.session.commit()
-
     if block3_data:
         for event_name, event_info in block3_data.items():
             event = Event(
                 name=event_name,
-                participants=event_info['participants'],
-                date=event_info['eventDate']
+                participants=event_info["participants"],
+                date_start=event_info["eventDate"],
             )
             db.session.add(event)
         db.session.commit()
 
+
 # Login page
-@app.route('/', methods=['GET'])
-def root():
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        login = request.form.get('login')
-        password = request.form.get('password')
+    """
+    Login page
+    """
+    if request.method == "POST":
+        login = request.form.get("login")
+        password = request.form.get("password")
 
-        if login == 'admin' and password == 'password':
-            session['user'] = login
-            return redirect(url_for('action'))
+        if login in users and users[login]["password"] == password:
+            user = User(login)
+            login_user(user)
+            session["user"] = login
+            return redirect(url_for("action"))
         else:
-            return "Неверный логин или пароль!"
+            flash("Неверный логин или пароль!")
 
-    return render_template('sign-in.html')
+    return render_template("sign-in.html")
 
-# Action selection page (fill in data/view data)
-@app.route('/action', methods=['GET'])
+
+@app.route("/logout")
+@login_required
+def logout():
+    """
+    Logout the user
+    """
+    logout_user()
+    flash("Вы вышли из системы")
+    return redirect(url_for("login"))
+
+
+@app.route("/action", methods=["GET"])
+@login_required
 def action():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-    return render_template('option.html')
+    """
+    Action selection page (fill in data/view data)
+    """
+    return render_template("option.html")
 
-# Processing the "Внести данные" button
-@app.route('/start-survey')
+
+@app.route("/start-survey")
+@login_required
 def start_survey():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-    return redirect(url_for('block1'))
+    """
+    Processing the "Внести данные" button
+    """
+    return redirect(url_for("block1"))
 
-# First block of questions
-@app.route('/block1', methods=['GET', 'POST'])
+
+@app.route("/block1", methods=["GET", "POST"])
+@login_required
 def block1():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
+    """
+    Processing the first block of questions
+    """
+    if request.method == "POST":
         data = {
-            'totalStudents': request.form.get('totalStudents'),
-            'class1': request.form.get('class1'),
-            'class2': request.form.get('class2'),
-            'class3': request.form.get('class3'),
-            'class4': request.form.get('class4'),
-            'class5': request.form.get('class5'),
-            'class6': request.form.get('class6'),
-            'class7': request.form.get('class7'),
-            'class8': request.form.get('class8'),
-            'class9': request.form.get('class9'),
-            'class10': request.form.get('class10'),
-            'class11': request.form.get('class11'),
+            "totalStudents": request.form.get("totalStudents"),
+            "class1": request.form.get("class1"),
+            "class2": request.form.get("class2"),
+            "class3": request.form.get("class3"),
+            "class4": request.form.get("class4"),
+            "class5": request.form.get("class5"),
+            "class6": request.form.get("class6"),
+            "class7": request.form.get("class7"),
+            "class8": request.form.get("class8"),
+            "class9": request.form.get("class9"),
+            "class10": request.form.get("class10"),
+            "class11": request.form.get("class11"),
         }
-        session['block1_data'] = data  # Save data in session
-        return redirect(url_for('block2'))
+        session["block1_data"] = data  # Save data in session
+        return redirect(url_for("block2"))
 
-    return render_template('block1.html')
+    return render_template("block1.html")
 
-# Second block of questions
-@app.route('/block2', methods=['GET', 'POST'])
+
+@app.route("/block2", methods=["GET", "POST"])
+@login_required
 def block2():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
+    """
+    Processing the second block of questions
+    """
+    if request.method == "POST":
         event_data = {
-            'event1': {
-                'event_name': request.form.get('event1'),
-                'participants': request.form.get('participants1'),
-                'date': request.form.get('date1')
+            "event1": {
+                "event_name": request.form.get("event1"),
+                "participants": request.form.get("participants1"),
+                "date": request.form.get("date1"),
             },
             # Todo: add other events
         }
-        session['block2_data'] = event_data  # Save data in session
+        session["block2_data"] = event_data  # Save data in session
+        if request.form.get("action") == "back":
+            return redirect(url_for("block1"))
+        elif request.form.get("action") == "next":
+            return redirect(url_for("block3"))
 
-        if request.form.get('action') == 'back':
-            return redirect(url_for('block1'))
-        elif request.form.get('action') == 'next':
-            return redirect(url_for('block3')) 
+    return render_template("block2.html")
 
-    return render_template('block2.html')
 
-# Third block of questions
-@app.route('/block3', methods=['GET', 'POST'])
+@app.route("/block3", methods=["GET", "POST"])
+@login_required
 def block3():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
+    """
+    Processing the third block of questions
+    """
+    if request.method == "POST":
         event_details = {
-            'eventName1': {
-                'name': request.form.get('eventName1'),
-                'participants': request.form.get('participants1'),
-                'eventDate': request.form.get('eventDate1')
+            "eventName1": {
+                "name": request.form.get("eventName1"),
+                "participants": request.form.get("participants1"),
+                "eventDate": request.form.get("eventDate1"),
             }
             # Todo: add other events
         }
-        session['block3_data'] = event_details
+        session["block3_data"] = event_details
+
         save_survey_data()
-        
-        if request.form.get('action') == 'back':
-            return redirect(url_for('block2'))
-        elif request.form.get('action') == 'submit':
-            return redirect(url_for('thanks'))
 
-    return render_template('block3.html')
+        if request.form.get("action") == "back":
+            return redirect(url_for("block2"))
+        elif request.form.get("action") == "submit":
+            return redirect(url_for("thanks"))
 
-# Thank you page
-@app.route('/thanks')
+    return render_template("block3.html")
+
+
+@app.route("/thanks")
+@login_required
 def thanks():
-    if 'user' not in session:
-        return redirect(url_for('login'))
+    """
+    "Thank you" page
+    """
     return render_template("thanks.html")
 
-# Processing the "Посмотреть данные" button
-@app.route('/view-data')
+
+@app.route("/view-data")
+@login_required
 def view_data():
+    """
+    Processing the "Посмотреть данные" button
+    """
     pass
 
-# School data page
-@app.route('/school-data', methods=['GET'])
+
+@app.route("/school-data", methods=["GET", "POST"])
+@login_required
 def school_data():
-    pass
+    """
+    School data page
+    """
+    if request.method == "POST":
+        return render_template(
+            "school-data.html",
+            class1=session["block1_data"]["class1"],
+            class2=session["block1_data"]["class2"],
+            class3=session["block1_data"]["class3"],
+            class4=session["block1_data"]["class4"],
+            class5=session["block1_data"]["class5"],
+            class6=session["block1_data"]["class6"],
+            class7=session["block1_data"]["class7"],
+            class8=session["block1_data"]["class8"],
+            class9=session["block1_data"]["class9"],
+            class10=session["block1_data"]["class10"],
+            class11=session["block1_data"]["class11"],
+        )
 
-# School comparison page
-@app.route('/compare-schools', methods=['GET'])
+
+@app.route("/compare-schools", methods=["GET"])
+@login_required
 def compare_schools():
+    """
+    School comparison page
+    """
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
